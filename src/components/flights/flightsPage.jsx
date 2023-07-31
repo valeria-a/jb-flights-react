6a55fb65908e6887a4d5fdb058e2375d7387bd7f
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-07-31 18:34:50 +0300

    more

--- src/components/flights/flightsPage.jsx
+++ src/components/flights/flightsPage.jsx
@@ -1,5 +1,26 @@
+import axios from "axios"
+import { useEffect } from "react"
+import * as urls from "../../infra/urls";
+
 export default function FlightsPage() {
+
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                try {
+                    const response = await axios.get(urls.FLIGHTS_LIST_URL)
+                    console.log(response)
+                } catch (e) {
+                    console.error(e)
+                }
+            }
+            fetchData()
+        }
+        ,[]
+    )
+
     return(
         <h2>Flights page</h2>
+
     )
 }
