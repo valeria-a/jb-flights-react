d02519fda816702edcdefdc901418095655ccf63
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-08-03 20:37:46 +0300

    mmm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header';
+import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { ME_URL } from './infra/urls';
+import { SetUserContext } from './context/userContext';
 
 function App() {
+
+  const setUser = useContext(SetUserContext)
+
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const token = localStorage.getItem('token')
+        if (token) {
+          const meResponse = await axios.get(ME_URL,
+            {headers: {Authorization: `Bearer ${token}`}})
+          console.log(meResponse)
+          setUser({
+            user: {...meResponse.data}
+          })
+        }
+      }
+      fetchData()
+    }
+  )
+
   return (
     <>
       <Header />
